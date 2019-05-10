eca2062f88954c9d66487126a5f8f637e18c5af5
aghaad <aghaad@live.fr>
2019-05-10 11:02:00 +0200

    suite du formulaire de base

--- src/Controller/BilletController.php
+++ src/Controller/BilletController.php
@@ -3,7 +3,15 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+use App\Entity\Booking;
+use App\Repository\BookingRepository;
+
 
 class BilletController extends AbstractController
 {
@@ -22,5 +30,39 @@ class BilletController extends AbstractController
      */
     public function home(){
         return $this->render('billet/home.html.twig');
+    }
+
+     /**
+     * @Route("/", name="home")
+     */
+    public function Booking(Request $request, ObjectManager $manager){
+        $Booking=new Booking();
+
+        $form=$this->createFormBuilder($Booking)
+                    ->add('prenom')
+                    ->add('nom')
+                    ->add('email')
+                    ->add('DateTime')
+                    ->add('nombre')
+                    ->add('type')
+                    ->add('dateDeNaissance')
+                    ->add('save',submitType::class,[
+                        'label'=> 'Enregistrer
+                    '])
+                    ->getForm();
+        
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->persist($Booking);
+            $manager->flush();
+/**
+ *return $this->render ('billet/.............à la nouvelle page,['id' => $Booking->getId ()]);
+ */
+
+        }
+             
+        return $this->render('billet/home.html.twig',[
+            'formBillet' => $form ->createView()
+        ]);
     }
 }
