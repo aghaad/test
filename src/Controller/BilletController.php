5155f6ec4548177a096e9525f1db8407bfeb207f
aghaad <aghaad@live.fr>
2019-05-17 13:53:58 +0200

    ajout controller infos et paiement

--- src/Controller/BilletController.php
+++ src/Controller/BilletController.php
@@ -85,7 +85,56 @@ class BilletController extends AbstractController
         return $this->render('billet/booking.html.twig', array('formBooking' => $form->createView()));    
     } 
 
+     /**
+     * @Route("/charge", name="charge")
+     */
+    public function charge(BookingRepository $repo, \Swift_Mailer $mailer, TicketRepository $repoticket, 
+    ServiceStripe $serviceStripe, ServiceMailer $serviceMailer)
+    {
+            $lastbooking = $repo->findBy([], ['id' => 'desc'],1,0);
+                $price = $lastbooking[0]->getTotalprice(); 
+                $number = $lastbooking[0]->getBookingnumber(); 
+                $email = $lastbooking[0]->getEmail();   
+                $date = $lastbooking[0]->getVisitDate();
+                $date = $date->format('d/m/Y'); 
+                $id = $lastbooking[0]->getId();
+           
+            $result = $serviceStripe->payment($price, $number);
 
+            if ($result == 'success') {
+                // $message = (new \Swift_Message('Votre paiement pour le Musée du Louvre'))
+                // ->setFrom('nesousx.website@gmail.com')
+                // ->setTo($email)
+                // ->setBody(
+                //     $this->render('louvre/registrations.html.twig', [
+                //     'date' => $date, 
+                //     'price' => $price, 
+                //     'number' => $number, 
+                //     'tickets' => $repoticket->findBy(['id' => $id])
+                //     ]
+                // ),
+                // 'text/html');
+                // $mailer->send($message);
+                $email = $serviceMailer->userConfirmation($email, $number, $date, $price, $repoticket, $id);
+                return $this->render('billet/charge.html.twig');             
+            }
+            else {
+                $this->addFlash(
+                    'danger',
+                    $message = 'Votre paiement n\'a pas fonctionné. Merci de recommencer'
+                    );  
+                return $this->render('billet/payment.html.twig', ['p' => $price]);
+            }
+            
+    }
+
+    /**
+     * @Route("/infos", name="infos")
+     */
+    public function infos()
+    {
+        return $this->render('billet/infos.html.twig');
+    }
 
 
 
