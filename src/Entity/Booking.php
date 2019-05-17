77165f9ab39559e384069ccb301279fc56e93aaa
aghaad <aghaad@live.fr>
2019-05-17 14:14:44 +0200

    bug  sur auth
    Remarque: propriété non définie: App \ Entity \ Booking :: $ tickets

--- src/Entity/Booking.php
+++ src/Entity/Booking.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -17,19 +16,19 @@ class Booking
      * @ORM\Column(type="integer")
      */
     private $id;
-
     /**
-     * @ORM\Column(type="string", length=30)
+     * @ORM\Column(type="string", length=255)
      */
     private $bookingnumber;
-
     /**
-     * @ORM\Column(type="string", length=30)
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Email(message="Votre email n'est pas valide")
      */
     private $email;
-
     /**
      * @ORM\Column(type="date")
+     * @Assert\NotBlank(message="Votre date de visite doit être renseigné")
+     * @Assert\Date
      */
     private $visitdate;
 
@@ -38,59 +37,56 @@ class Booking
      */
     private $totalprice;
 
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Ticket", mappedBy="booking", orphanRemoval=true, cascade = {"persist"})
+     * @Assert\Valid
+     */
+    protected $tickets;
+
+    public function __construct()
+    {
+        $this->tickets = new ArrayCollection();
+    }
     public function getId(): ?int
     {
         return $this->id;
     }
-
     public function getBookingnumber(): ?string
     {
         return $this->bookingnumber;
     }
-
     public function setBookingnumber(string $bookingnumber): self
     {
         $this->bookingnumber = $bookingnumber;
-
         return $this;
     }
-
     public function getEmail(): ?string
     {
         return $this->email;
     }
-
     public function setEmail(string $email): self
     {
         $this->email = $email;
-
         return $this;
     }
-
     public function getVisitdate(): ?\DateTimeInterface
     {
         return $this->visitdate;
     }
-
-    public function setVisitdate(\DateTimeInterface $visitdate): self
+    public function setVisitdate(\DateTime $visitdate=null): self
     {
         $this->visitdate = $visitdate;
-
         return $this;
     }
-
     public function getTotalprice()
     {
         return $this->totalprice;
     }
-
     public function setTotalprice($totalprice): self
     {
         $this->totalprice = $totalprice;
-
         return $this;
     }
-
     /**
      * @return Collection|Ticket[]
      */
@@ -116,6 +112,4 @@ class Booking
         }
         return $this;
     }
-
-
 }
