34724432aa406ad7035fe0d0d7da20cf177fc706
aghaad <aghaad@live.fr>
2019-05-25 20:11:38 +0200

    Bug sur l'enregistrement donc réinitialisation

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-p4
+# Louvre
+Version
+1.0.0
+
+Langage
+PHP avec Symfony 4.1
+
+URL
+https://www.projets-opc-nana.com/
+
+- Un formulaire de réservation en ligne 
+- Gestion des réservation en fonction des dates  
+- Deux types de billets : journée et demi-journée 
+- Gestion des prix en fonction de l’âge du visiteur 
+- Paiement en ligne 
+- Envoi d’un mail de confirmation 
+- Gestion du nombre de visiteurs par date 
+ 
+ 
---
+++ src/Services/StripePayment.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace App\Services;
+
+/**
+ * Description of Stripe
+ *
+ * @author djapa
+ */
+class StripePayment {
+    
+    public function StripePayment($prixTTC, $token) {
+
+        try {
+            \Stripe\Stripe::setApiKey("pk_test_OETqWICpPFb6Rwv8C3GXF7AH00JJanRIcx");
+
+
+            // Token is created using Checkout or Elements!
+            // Get the payment token ID submitted by the form:
+
+           return \Stripe\Charge::create([
+                'amount' => $prixTTC,
+                'currency' => 'eur',
+                'description' => 'Paiement final',
+                'source' => $token,
+                'receipt_email' => 'aghaad@live.fr',
+            ]);
+        } catch (\Exception $e) {
+            
+            return false;
+          
+        }
+    }
+}
